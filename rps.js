93c7f776de1e35124e71e1b678f82edc7ec0309f
Aayam Bhattarai <aayamgt@hotmail.com>
2023-06-09 15:05:02 +0545

    Refactored code to using UI to play the game (Score Tracking not working)

--- rps.js
+++ rps.js
@@ -5,33 +5,68 @@
 // keep score (Remaining);
 function getComputerChoice() {
   const choice = ['Rock', 'Paper', 'Scissor'];
-  let   random = Math.floor(Math.random() * (2-0+1)) + 0;
+  let random = Math.floor(Math.random() * (2-0+1)) + 0;
   return choice[random];
+}
+
+function Results(number) {
+  const container = document.querySelector('#container');
+  const div = document.createElement('div');
+  const score = document.createElement('div');
+  let computerScore = 0;
+  let playerScore = 0;
+  if (number == 0) {
+   div.textContent = 'The Match Ends in a Draw !!! '; 
+   score.textContent = `Player Score : ${playerScore}  Computer Score: ${computerScore}`
+   container.appendChild(div);
+   container.appendChild(score);
+  } else if (number == 1) {
+      computerScore = computerScore + 1;
+      score.textContent = `Player Score : ${playerScore}  Computer Score: ${computerScore}`
+      div.textContent = 'Too Bad You Loose the Match !!!';
+      container.appendChild(div);
+      container.appendChild(score);
+  } else if (number == 2){
+     playerScore = playerScore + 1;
+     score.textContent = `playerScore : ${playerScore}  computerScore: ${computerScore}` 
+     div.textContent = 'Congratulations You Win the Match !!!';
+     container.appendChild(div);  
+     container.appendChild(score);
+  }
 }
 
 function playround(playerSelection, computerSelection){
  let computerChoice = computerSelection.toLowerCase(); 
+ let id = 0;
  if(playerSelection == computerChoice){
-  return console.log('The game is draw');
- } else if ((computerChoice == 'rock' && playerSelection == 'scissor') ||
+  let id = 0;
+  return id; 
+  } else if ((computerChoice == 'rock' && playerSelection == 'scissor') ||
     (computerChoice == 'paper' && playerSelection == 'rock') ||
     (computerChoice == 'scissor' && playerSelection == 'paper')){
-  return console.log('Too Bad!! You Loose!');
+    let id = 1;
+    return id;
   } else {
-    console.log('Congratulation!! You Win!');
+    let id = 2;
+    return id;
   }
 }
 
-function game() {
-  for (let index = 1; index <= 5; index++) {  
-    let playerSelection = prompt('Rock, Paper, Scissor Shoot!!!').toLowerCase();
-    let computerSelection = getComputerChoice();
-    console.log(computerSelection);
-    playround(playerSelection, computerSelection);
-  }  
-}
+const buttons = document.querySelectorAll('button');
+let playerSelection = '';
+let computerChoice = '';
+let roundInformation = 0;   
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let playerSelection = button.id.toLowerCase();
+    console.log('Your Selection is : ' + playerSelection);
+    let computerChoice = getComputerChoice();
+    console.log("Computer's Selection is :" + computerChoice);
+    let roundInformation = playround(playerSelection, computerChoice);
+    console.log(roundInformation);
+    Results(roundInformation);
+  });
+});
 
 
-// function call for game 
-game();
 
